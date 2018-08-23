59e2ba91cdc9e09ba1927eb19d3bec4776888b2a
EC2 Default User <ec2-user@ip-172-31-20-181.us-east-2.compute.internal>
2018-08-23 14:43:30 +0000

    adding comments

--- hw2/tree/tree.py
+++ hw2/tree/tree.py
@@ -1,30 +1,38 @@
 class Tree(object):
 
     def __init__(self, root):
+        """Initialize the root
+        """
         self.root = root
 
     def get_value_root(self):
+        """Get the root value of the tree
+        """
         if self.root is not None:
             return self.root.value
         else:
             return None
 
     def printTree(self):
+        """Get the list of list for a tree
+        """
         h = self.getDepth(self.root)
+        #the maximum code for depth of h
         r = 2**h - 1
+        #Initialize the the list of list
         lst = [["|" for i in range(r)] for j in range(h)]
         lst = self.getAns(self.root, lst, h - 1, 2**(h - 1) - 1)
         return lst
-        # for col in lst:
-        #     for e in col:
-        #         print(e, end="")
-        #     print()
 
     def getAns(self, root, lst, level, pos):
-        # print("level: " + str(level))
-        # print("pos: " + str(pos))
+        """Modify "|" in the list of list to the value of the
+        node. 
+        """
         if isinstance(root, Node):
+            #Change to the value of a node starting from the 1st level
             lst[-level - 1][pos] = str(root.value)
+            #Go the next level and adjusting the position based on if it is
+            #on the left or right
             if root.left or root.left == 0:
                 self.getAns(root.left, lst, level - 1, pos - 2 ** (level - 1))
             if root.right or root.right == 0:
@@ -34,6 +42,8 @@ class Tree(object):
         return lst
 
     def getDepth(self, root):
+        """Recursively calculate the depth of a tree
+        """
         if root is not None and isinstance(root, Node):
             return 1 + max(self.getDepth(root.left), self.getDepth(root.right))
         elif root is None:
@@ -45,6 +55,8 @@ class Tree(object):
 class Node(object):
 
     def __init__(self, value, left, right):
+        """Initialize the node
+        """
         self.value = value
         self.left = left
         self.right = right
